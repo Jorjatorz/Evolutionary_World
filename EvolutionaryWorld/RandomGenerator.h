cc67a2888c9f03b2adf0514baa422885adb25112
Jorge Sanchez <jorjatorz@gmail.com>
2018-03-19 00:45:30 +0100

    Added a global generator and a gaussian distribution generator

--- EvolutionaryWorld/RandomGenerator.h
+++ EvolutionaryWorld/RandomGenerator.h
@@ -14,7 +14,11 @@ public:
 	// Generates a random integer between [min, max)
 	float randomFloat(float min = 0.0, float max = 1.0);
 
+	// Generates a number from a gaussian distribution
+	float gaussianFloat(float mean, float std);
+
 private:
-	static boost::mt19937 generator;
+	static boost::mt19937 global_generator; // Geenrator in charge of generating local generators seed
+	boost::mt19937 generator;
 };
 
